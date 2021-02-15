22b5f054c48fd2bb9f02f90f83eba41520039f1f
Dalton Klein <deklein@live.com>
2021-02-15 06:27:22 -0600

    Fix/AboutMeSectionReworks

--- dklein.io/src/components/aboutmeComponent.js
+++ dklein.io/src/components/aboutmeComponent.js
@@ -12,14 +12,23 @@ export default function AboutmeComponent(props) {
       <div className={`${styles.aboutmeheading}
       ${props.checked == false ? styles.textlight : styles.textdark}`}
       >
-      🌈Welcome to my personal website🌈 where I host my latest development endeavors 😊
+      😊Welcome to my personal website, where I host my latest development endeavors😊
       </div>
       <div className={`${styles.aboutmetext}
       ${props.checked == false ? styles.textlight : styles.textdark}`}
       >
         I am a full stack developer living in Minneapolis, MN. In a past life, I obtained a finance 
         degree from the U of M Duluth and worked in corporate accounting. When Covid-19 struck, I knew 
-        it was time to take my passion for coding to the next level.   
+        it was time to take my passion for coding to the next level. So, I decided to devote my 
+        full attention to learning as many new tools and frameworks as possible in the JavaScript 
+        environment. 
+      </div>
+      <div className={`${styles.aboutmetext}
+      ${props.checked == false ? styles.textlight : styles.textdark}`}
+      >
+        I studied very hard and got accepted into the best and most advanced coding program
+        that I could find, Codeworks. Even when their US cohort was cancelled due to the virus, I did not
+        let that stop me, and I joined the EU remote cohort studying from 2AM - 2PM daily. 
       </div>
     </div>
   )
